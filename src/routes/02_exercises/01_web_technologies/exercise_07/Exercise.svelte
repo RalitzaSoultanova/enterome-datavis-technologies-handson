af0a34fae9e41edd65f152b5b52312ef87a3e059
RalitzaSoultanova <ralitza.soultanova@gmail.com>
2023-03-18 18:58:04 +0100

    Ralitza

--- src/routes/02_exercises/01_web_technologies/exercise_07/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_07/Exercise.svelte
@@ -1,1 +1,6 @@
-My lucky number is
+<script>
+	const my_variable = 70;
+</script>
+
+
+My lucky number is {my_variable}
---
+++ src/routes/02_exercises/01_web_technologies/exercise_08/Exercise.svelte
@@ -0,0 +1,6 @@
+<script>
+const my_list = ["apples", "oranges", "peaches", "wattermelon"]
+const my_name = Raly;
+</script>
+
+My name is {my_name} and my favorite fruits are {my_list}
